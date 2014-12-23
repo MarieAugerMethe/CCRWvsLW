3e1ba6b43471ccd0ac13e4c584c9b7d9747a782f
Marie Auger-Methe <marie.augermethe@gmail.com>
2014-12-23 10:47:27 -0400

    Changing pseudoresiduals figures

--- R/pseudoResFx.R
+++ R/pseudoResFx.R
@@ -23,31 +23,29 @@
 # 3. I'm using a shapiro test of normality to test whether the normal 
 # pseudo-residuals are normal
 
-# library(nortest)
-
 ################################################################
 # Functions needed
 
 ###
 # test of uniformity based on G-test
 
-pseudo.u.test <- function(U, nP, n, graph){
+pseudo.u.test <- function(U, nP, n, graph, movMes){
 
   # Binning the uniform pseudo residuals
-  # Divinding the uniform distirbution in as many bins
+  # Dividing the uniform distribution in as many bins as possible
   # that will have 5 expected values.
-  # SInce the degrees of freedom depends on the number
+  # Since the degrees of freedom depends on the number
   # of bins and the number of parameters estimated
   # df = nB - nP -1
   # and the biggest number of parameter estimated is 3
   # we need a minium of 5 bins with 10 expected
   # so a minimum of 50 data points.
   
-  #Not constant size bin
+  # Not constant size bin
   nB <- floor(n/10)
   U_E <- n/nB
   U_B <- seq(0,1,length.out=(nB+1))
-  U_B <- hist(U,breaks=U_B,plot=F)$counts
+  U_B <- hist(U,breaks=U_B,plot=FALSE)$counts
   
   ##
   # The actual G-test
@@ -73,15 +71,14 @@ pseudo.u.test <- function(U, nP, n, graph){
   # q=1+(a^2-1)/6* n*dfGT, where a is number of category
   wc <- 1 + (nB^2 -1)/(6 * n * dfGT)
   
-  pValGT <- pchisq(GVal/wc,dfGT,lower.tail=F)
+  pValGT <- pchisq(GVal/wc,dfGT,lower.tail=FALSE)
   pValGT <- rbind(Gcor=(GVal)/wc,pValGT)
   
-  
-  if(graph==T){
-    #windows()
+  if(graph==TRUE){
     par(mfrow=c(1,2))
-    hist(U, nB)
-    acf(U)
+    hist(U, nB, xlab = "Uniform pseudo-residuals", ylab=paste("Frequency of",movMes), main="")
+    box()
+    acf(U, xlab="Lag (steps)", ylab=paste("ACF",movMes), main="")
   }
   return(pValGT)
 }
@@ -138,16 +135,16 @@ pseudo <- function(SL,TA_C,TA,SLmin,SLmax,missL,notMisLoc,n,mleMov,PRdetails,gra
 	##########################################################
 	# Pseudo residuals
 
-  # If graph=T get the best model
+  # If graph=TRUE get the best model
   # since only the pseudo residuals of the best model are shown
   if(graph){
-    bestM <- grep("AIC",names(unlist(mleMov)))
+    bestM <- grep("AICc",names(unlist(mleMov)))
     # Order: CCRW, LW, TLW, BW, CRW, TBW, TCRW
     bestM <- which.min(unlist(mleMov)[bestM])
-    graph <- rep(F,7)
-    graph[bestM] <- T
+    graph <- rep(FALSE,7)
+    graph[bestM] <- TRUE
   }else{
-    graph <- rep(F,7)
+    graph <- rep(FALSE,7)
   }
   
   # By definition the SL that are corresponding to SLmin and SLmax
@@ -163,7 +160,6 @@ pseudo <- function(SL,TA_C,TA,SLmin,SLmax,missL,notMisLoc,n,mleMov,PRdetails,gra
   colnames(Z) <- c('SL_CCRW', 'SL_LW', 'SL_TLW','SL_E','SL_TE',
                    'TA_CCRW', 'TA_U','TA_VM')
   rownames(Z) <- c('W','pval')
-  
   
 	##
 	# CCRW
@@ -185,14 +181,13 @@ pseudo <- function(SL,TA_C,TA,SLmin,SLmax,missL,notMisLoc,n,mleMov,PRdetails,gra
     # each set of pseudo-residuals.
     # SO for SL, you have 3 parameters: minSL, lambda_T, lambda_F
     # SO for TA, you have 1 parameter: kappa_T
-    Z[,1] <- pseudo.u.test(U_SL_CCRW, 3, n, graph[1])
-    Z[,6] <- pseudo.u.test(U_TA_CCRW, 1, n, graph[1])
+    Z[,1] <- pseudo.u.test(U_SL_CCRW, 3, n, graph[1], "SL")
+    Z[,6] <- pseudo.u.test(U_TA_CCRW, 1, n, graph[1], "TA")
   }
 
   
   #####
   # SL
-  
   
 	##
 	# LW
@@ -244,21 +239,21 @@ pseudo <- function(SL,TA_C,TA,SLmin,SLmax,missL,notMisLoc,n,mleMov,PRdetails,gra
 	# SL
 	
   # nP = SLmin + mu
-	Z[,2] <- pseudo.u.test(U_LW,2,n,graph[2])
+	Z[,2] <- pseudo.u.test(U_LW,2,n,graph[2],"SL")
   # nP = SLmin + mu + SLmax
-	Z[,3] <- pseudo.u.test(U_TLW,3,n,graph[3])
+	Z[,3] <- pseudo.u.test(U_TLW,3,n,graph[3],"SL")
   # nP = SLmin + lambda
-	Z[,4] <- pseudo.u.test(U_E,2,n,graph[4]|graph[5])
+	Z[,4] <- pseudo.u.test(U_E,2,n,graph[4]|graph[5],"SL")
   # nP = SLmin + lambda + SLmax
-	Z[,5] <- pseudo.u.test(uTE,3,n,graph[6]|graph[7])
+	Z[,5] <- pseudo.u.test(uTE,3,n,graph[6]|graph[7],"SL")
 
 	######################
 	# TA
   
   # nP = nothing!
-	Z[,7] <- pseudo.u.test(U_U,0,n,graph[2]|graph[3]|graph[4]|graph[6])
+	Z[,7] <- pseudo.u.test(U_U,0,n,graph[2]|graph[3]|graph[4]|graph[6],"TA")
   # nP = kapa
-	Z[,8] <- pseudo.u.test(U_VM,1,n,graph[5]|graph[7])
+	Z[,8] <- pseudo.u.test(U_VM,1,n,graph[5]|graph[7],"TA")
 
 	PR <- matrix(NA,2,7)
 	colnames(PR) <- c('CCRW', 'LW', 'TLW', 'BW', 'CRW', 'TBW', 'TCRW')
